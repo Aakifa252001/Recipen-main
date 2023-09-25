b66220fad704f57a023f653afe95b09ace8e4866
Aakifa252001 <aakifapocha99@gmail.com>
2023-09-25 11:04:50 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,46 @@
+require("dotenv").config();
+// const cors = require("cors");
+// const corsOptions = require("./config/corsOptions");
+const express = require("express");
+const errorHandler = require("./middleware/errorHandler");
+const credentials = require("./middleware/credentials");
+const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
+const connectDB = require("./db/conn");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// cors middleware
+// app.use(credentials);
+// app.use(cors(corsOptions));
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header("Access-Control-Allow-Credentials",'true')
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+app.use(express.urlencoded({ extended: false }));
+
+app.use(cookieParser());
+
+app.use(express.json());
+
+// route middleware
+app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api/user", require("./routes/userRoutes"));
+app.use("/api/recipe", require("./routes/recipeRoutes"));
+app.use("/api/blog", require("./routes/blogRoutes"));
+app.use("/api/stripe", require("./routes/subscriptionRoutes"));
+
+app.use(errorHandler);
+
+connectDB()
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(port, () => console.log(`Server running on port ${port}`));
+  })
+  .catch((err) => {
+    console.error(`Error connecting to MongoDB ${err}`);
+  });
